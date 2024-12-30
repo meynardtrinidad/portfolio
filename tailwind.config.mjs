107bf6f6289457bfe4f2725270c730c7959c4c7a
Meynard Trinidad <meynard.trinidad@diversifiedinfra.com>
2024-12-30 21:59:52 +0800

    feat: minor update in button design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,32 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-        extend: {
-            colors: {
-                project: {
-                    gray: {
-                        50: "#F8F8F8",
-                        100: "#F2F2F2",
-                        200: "#DCDCDC",
-                        300: "#BDBDBD",
-                        400: "#989898",
-                        500: "#7C7C7C",
-                        600: "#656565",
-                        700: "#525252",
-                        800: "#464646",
-                        900: "#3D3D3D",
-                        950: "#292929",
-                    }
-                }
-            },
-            boxShadow: {
-                "project-button": `inset 0 2px 0 0 rgb(255 255 255 / 0.25), 0 1.5px 2px -1px rgb(0 0 0 / 0.25)`,
-            },
-            screens: {
-                xs: "320px",
-            },
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        project: {
+          gray: {
+            50: "#F8F8F8",
+            100: "#F2F2F2",
+            200: "#DCDCDC",
+            300: "#BDBDBD",
+            400: "#989898",
+            500: "#7C7C7C",
+            600: "#656565",
+            700: "#525252",
+            800: "#464646",
+            900: "#3D3D3D",
+            950: "#292929",
+          },
         },
+      },
+      boxShadow: {
+        "project-button": `inset 0 3px 1px -2px rgba(255, 255, 255, 0.18), 0 1.5px 2px -1px rgba(0, 0, 0, 0.25)`,
+      },
+      screens: {
+        xs: "320px",
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
